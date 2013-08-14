7fcbefeca7562b78997ffca30dda8f55519807fc
DBC-Apprentice <dbc@dbc.com>
2013-08-13 17:36:31 -0700

    some progress...

--- application.js
+++ application.js
@@ -1,15 +1,49 @@
-$(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
-  });
+// $(document).ready(function() {
+//   $('#roller button.add').on('click', function() {
+//     console.log("WAT")
+//     $('.dice').append('<div class="die">0</div>');
+//   });
 
-  $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
-  });
+//   $('#roller button.roll').on('click', function() {
+//     $('.die').each(function(k, die) {
+//       var value = Math.floor((Math.random()*6)+1);
+//       $(die).text(value);
+//     });
+//   });
+// });
+
+
+function Die() {
+    this.value = 0; // starting value
+  }
+
+  Die.prototype.roll = function() {
+    this.value = Math.floor((Math.random()*6)+1);
+  };
+
+  Die.prototype.render = function () {
+    return '<div class="die">'+this.value+'</div>';
+  };
+
+  function DiceHolder(){
+    this.dice = []
+  }
+
+  DiceHolder.prototype.add_dice=function(){
+
+    this.dice.push(new Die())
+  }
+
+
+$(document).ready(function(){
+
+ var  my_dice= new DiceHolder();
+  
+$('#roller button.add').on('click', function() {
+ my_dice.add_dice();
+  $(".dice").append(die.render());
 });
 
+  
 
+});
